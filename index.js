4b8f2e26170a735e433bb3fe90f8dccc5da0b408
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-07-17 17:29:42 +0900

    コールバック関数の例

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-function sleep(ms) {
-  const startTime = new Date();
+// function sleep(ms) {
+//   //const startTime = new Date();
+//   // while (new Date() - startTime < ms );
 
-  while (new Date() - startTime < ms );
+//   setTimeout(function () {
+//     alert("sleep関数を完了しました。");
+//   }, ms);
 
-  alert("sleep関数が完了しました")
+//   alert("sleep関数が完了しました");
+// }
 
-}
+// // sleep(3000);
 
-// sleep(3000)
+// const btn = document.querySelector("button");
+// function clickHandler() {
+//   alert("ボタンがクリックされました。");
+// }
+// btn.addEventListener("click", clickHandler);
 
-const btn = document.querySelector("button");
-function clickHandler() {
-  alert("ボタンがクリックされました。")
-}
-btn.addEventListener("click", clickHandler);
+let val = 0;
 
+setTimeout(() => {
+  val = 1;
+  console.log(val);
+}, 0);
 
+console.log(val);
